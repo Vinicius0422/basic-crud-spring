df75c3775f2a4ab0e3722fcc1317b9b2e0b081ea
Vinicius0422 <guedescosta123@hotmail.com>
2024-03-10 17:38:25 -0300

    feat: adding Contact Dto

--- src/main/java/br/com/vinicius/basiccrud/dtos/ContactDto.java
+++ src/main/java/br/com/vinicius/basiccrud/dtos/ContactDto.java
@@ -1,2 +1,41 @@
-package br.com.vinicius.basiccrud.dtos;public record ContactDto() {
-}
+package br.com.vinicius.basiccrud.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+
+public record ContactDto(
+        @NotBlank
+        @Max(50)
+        String firstName,
+        @NotBlank
+        @Max(50)
+        String lastName,
+        @NotBlank
+        @Max(3)
+        Integer age,
+        @NotBlank
+        @Max(150)
+        @Email
+        String email,
+        @NotBlank
+        @Max(50)
+        String phone,
+        LocalDate birthDate,
+        String occupation,
+        String company,
+        @NotBlank
+        @Max(150)
+        String street,
+        @NotBlank
+        @Max(50)
+        String city,
+        @NotBlank
+        @Max(50)
+        String neighborhood,
+        @NotBlank
+        @Max(15)
+        String zipCode
+) { }
