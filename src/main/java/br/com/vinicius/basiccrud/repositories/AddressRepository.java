026a1c0a1736197942864fd57885e151ee4537c7
Vinicius0422 <guedescosta123@hotmail.com>
2024-03-10 17:10:04 -0300

    refactor: add @Repository annotation to repositories

--- src/main/java/br/com/vinicius/basiccrud/repositories/AddressRepository.java
+++ src/main/java/br/com/vinicius/basiccrud/repositories/AddressRepository.java
@@ -2,6 +2,8 @@ package br.com.vinicius.basiccrud.repositories;
 
 import br.com.vinicius.basiccrud.models.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
--- src/main/java/br/com/vinicius/basiccrud/repositories/ContactRepository.java
+++ src/main/java/br/com/vinicius/basiccrud/repositories/ContactRepository.java
@@ -2,6 +2,8 @@ package br.com.vinicius.basiccrud.repositories;
 
 import br.com.vinicius.basiccrud.models.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 }
