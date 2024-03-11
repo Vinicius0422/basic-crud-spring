d325183cc0d497c72ab51830ed200d5452052afb
Vinicius0422 <guedescosta123@hotmail.com>
2024-03-10 21:47:47 -0300

    refactor: refactor Contact

--- src/main/java/br/com/vinicius/basiccrud/models/Contact.java
+++ src/main/java/br/com/vinicius/basiccrud/models/Contact.java
@@ -23,9 +23,9 @@ public class Contact {
     private String lastName;
     @Column(nullable = false, length = 3)
     private Integer age;
-    @Column(nullable = false, length = 150)
+    @Column(nullable = false, length = 150, unique = true)
     private String email;
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 50, unique = true)
     private String phone;
     private LocalDate birthDate;
     private String occupation;
@@ -33,9 +33,29 @@ public class Contact {
     @Column(length = 150)
     private String street;
     @Column(length = 50)
+    private String country;
+    @Column(length = 50)
     private String city;
     @Column(length = 50)
     private String neighborhood;
     @Column(length = 15)
     private String zipCode;
+
+    public Contact(String firstName, String lastName, Integer age, String email, String phone,
+                   LocalDate birthDate, String occupation, String company, String street, String country, String city,
+                   String neighborhood, String zipCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.occupation = occupation;
+        this.company = company;
+        this.street = street;
+        this.country = country;
+        this.city = city;
+        this.neighborhood = neighborhood;
+        this.zipCode = zipCode;
+    }
 }
